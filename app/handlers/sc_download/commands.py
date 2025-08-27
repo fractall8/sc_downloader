a3bfb4d31bf61cd7c4161243af34ee3f8479f828
efkdk <dovbikivan2@gmail.com>
2025-08-27 15:33:35 +0300

    refactor: improve user feedback messages

--- app/handlers/sc_download/commands.py
+++ app/handlers/sc_download/commands.py
@@ -38,16 +38,23 @@ async def process_track_url(message: Message, state: FSMContext):
     try:
         state_data = await state.get_data()
         track_url_input = state_data.get("track_url")
+
         if track_url_input is None:
             await message.answer("Track url not provided")
             return
+
+        if not "soundcloud.com" in track_url_input:
+            await message.answer(
+                "This doesn't appear to be a SoundCloud link. Please provide the correct URL."
+            )
+            return
+
+        loading_state_message = await message.answer("Checking your link...")
         track_url = await resolve_soundcloud_url(short_url=track_url_input)
 
         client_id = await get_client_id_cached()
         track_info = await get_track_info(client_id=client_id, track_url=track_url)
-        await message.answer(
-            f"Starting downloading track... Provided track: {html.link(link=track_url, value=f"{track_info["title"] or "your track"}")}"
-        )
+        await loading_state_message.edit_text("Downloading audio...")
 
         # check if file name is safe for file system
         title_safe = "".join(
@@ -57,25 +64,27 @@ async def process_track_url(message: Message, state: FSMContext):
 
         if track_info.get("downloadable") and "download_url" in track_info:
             download_url = f"{track_info['download_url']}?client_id={client_id}"
-            await message.answer("⬇️ Downloading via official download_url")
             file_bytes = await get_file(
                 track_id=track_info["id"], url=download_url, filename=filename
             )
         else:
             stream_url = await get_stream_url(track=track_info, client_id=client_id)
             if stream_url:
-                await message.answer("⬇️ Downloading via progressive stream")
                 file_bytes = await get_file(
                     track_id=track_info["id"], url=stream_url, filename=filename
                 )
             else:
                 await message.answer(
-                    "❌ Could not find a downloadable link for this track"
+                    "Could not find a downloadable link for this track :("
                 )
                 return
 
+        await loading_state_message.edit_text("Done, sending your file ;)")
         await message.reply_audio(BufferedInputFile(file=file_bytes, filename=filename))
+        await loading_state_message.delete()
 
     except Exception as e:
         logger.error("Error while downloading track: %s", e)
-        await message.answer(f"Failed to download your track :(")
+        await message.answer(
+            f"Oops, failed to download track. Maybe the link is incorrect or the track does not have available audio. Try again or if the error persists another URL."
+        )
